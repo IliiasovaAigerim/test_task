ef2dd876ef4a4b201623ad43091226487930a5d0
Gesha <iliyasovaaika@gmail.com>
2025-12-05 22:24:16 +0600

    resolving issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         authentication_keys: [:login]
+         authentication_keys: [ :login ]
 
   validates :login, presence: true, uniqueness: true
   validates_format_of :login, with: URI::MailTo::EMAIL_REGEXP
