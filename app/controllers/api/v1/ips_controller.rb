57ea844be11b7aab78d7c1d49e52ed9ec3140f9c
Gesha <iliyasovaaika@gmail.com>
2025-12-14 20:12:51 +0600

    creating ips with multiple authors endpoint and writing specs

---
+++ app/controllers/api/v1/ips_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::IpsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def multiple_authors
+    ips = Post.group(:ip).having('COUNT(DISTINCT(user_id)) > 1').pluck(:ip)
+    users_with_ips = User.joins(:posts)
+                         .where('posts.ip': ips)
+                         .select('users.login', 'posts.ip')
+    ips_with_logins = users_with_ips.each_with_object({}) do |record, hash|
+      ip = record.ip
+      login = record.login
+
+      hash[ip] ||= []
+      hash[ip] << login
+    end
+    serializer = MultipleAuthorsIpsSerializer.new(ips_with_logins)
+    render json: serializer.as_json
+  end
+end
+
+
+
