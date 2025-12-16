81006a34613b3117d455c41faf5ccd600306899b
Gesha <iliyasovaaika@gmail.com>
2025-12-16 23:52:16 +0600

    fixing ci.yml file

--- app/controllers/api/v1/ips_controller.rb
+++ app/controllers/api/v1/ips_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::IpsController < ApplicationController
   def multiple_authors
-    ips = Post.group(:ip).having('COUNT(DISTINCT(user_id)) > 1').pluck(:ip)
+    ips = Post.group(:ip).having("COUNT(DISTINCT(user_id)) > 1").pluck(:ip)
     users_with_ips = User.joins(:posts)
                          .where('posts.ip': ips)
-                         .select('users.login', 'posts.ip')
+                         .select("users.login", "posts.ip")
     ips_with_logins = users_with_ips.each_with_object({}) do |record, hash|
       ip = record.ip
       login = record.login
@@ -15,6 +15,3 @@ class Api::V1::IpsController < ApplicationController
     render json: serializer.as_json
   end
 end
-
-
-
--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -13,8 +13,8 @@ class Api::V1::PostsController < ApplicationController
     )
     if post.save
       render json: {
-        post: post.as_json(only: [:id, :title, :body, :ip, :user_id, :created_at]),
-        user: user.as_json(only: [:id, :login])
+        post: post.as_json(only: [ :id, :title, :body, :ip, :user_id, :created_at ]),
+        user: user.as_json(only: [ :id, :login ])
       }, status: :created
     else
       render json: { errors: post.errors.full_messages }, status: :unprocessable_entity
@@ -25,6 +25,6 @@ class Api::V1::PostsController < ApplicationController
     n = params[:n].to_i
     return render json: { error: "N must be a positive integer." }, status: :bad_request unless n > 0
     posts = Post.order(average_rating: :desc).limit(n)
-    render json:posts, each_serializer: TopPostSerializer
+    render json: posts, each_serializer: TopPostSerializer
   end
 end
